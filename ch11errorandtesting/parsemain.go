d2bcd3b1b297b73a99fd47911a7b7c3415d159e8
David Dansby <ddansby89@gmail.com>
2020-08-24 02:22:25 -0700

    Add error handling & panicking in user defined pkg

---
+++ ch11errorandtesting/parsemain.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DataDavD/waytogocourse/ch11errorandtesting/parse"
+)
+
+func main() {
+	var examples = []string{
+		"1 2 3 4 5",
+		"100 50 25 12.5 6.25",
+		"2 + 2 = 4",
+		"1st class",
+		"",
+	}
+
+	for _, ex := range examples {
+		fmt.Printf("Parsing %q:\n  ", ex)
+		nums, err := parse.Parse(ex)
+		if err != nil {
+			fmt.Println(err) // here String() method from ParseError is used
+			continue
+		}
+		fmt.Println(nums)
+	}
+}
