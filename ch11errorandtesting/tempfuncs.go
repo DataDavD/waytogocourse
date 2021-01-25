9afbe3e4f23b677029bcce7a577a664dcc018cb7
David Dansby <ddansby89@gmail.com>
2021-01-25 02:22:05 -0800

    Playing with func chaining in templates

--- ch11errorandtesting/tempfuncs.go
+++ ch11errorandtesting/tempfuncs.go
@@ -9,6 +9,9 @@ func main() {
 	t := template.New("test")
 	t = template.Must(t.Parse("{{with $x := `hello`}} {{printf `%s %s` $x `Mary`}} {{end}}!\n"))
 	t.Execute(os.Stdout, nil)
-	t = template.Must(t.Parse(`{{with $x := "david"}} The length of the word {{.}} is {{len .}} {{end}}`))
+	t = template.Must(t.Parse(`{{with $x := "david"}} The length of the word {{.}} is {{len .}} {{end}}
+`))
+	t.Execute(os.Stdout, nil)
+	t = template.Must(t.Parse(`{{"output david" | printf "%s" | printf "%q"}}`))
 	t.Execute(os.Stdout, nil)
 }
