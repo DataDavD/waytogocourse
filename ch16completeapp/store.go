04528984f292460aaf177005b1ae05a9c0652580
David Dansby <ddansby89@gmail.com>
2021-02-17 01:22:09 -0800

    Added URLStore data structure and initial methods

---
+++ ch16completeapp/store.go
@@ -0,0 +1,45 @@
+package main
+
+import "sync"
+
+type URLStore struct {
+	urls map[string]string // map from short to long URLs
+	mu   sync.RWMutex
+}
+
+func NewURLStore() *URLStore {
+	return &URLStore{urls: make(map[string]string)}
+}
+
+func (s *URLStore) Get(key string) string {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.urls[key]
+}
+
+func (s *URLStore) Set(key, url string) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, present := s.urls[key]; present {
+		return false
+	}
+	s.urls[key] = url
+	return true
+}
+
+func (s *URLStore) Count() int {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return len(s.urls)
+}
+
+func (s *URLStore) Put(url string) string {
+	for {
+		key := genKey(s.Count()) // TODO: genKey to be added
+		if s.Set(key, url) {
+			return key
+		}
+	}
+	// shouldn't get to here
+	return ""
+}
